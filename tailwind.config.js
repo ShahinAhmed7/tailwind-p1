df6ee76754fa0a61fa2dd9d9d4590bf2e55d1f20
Shahin <meshahin007@gmail.com>
2020-12-13 11:47:25 +0600

    Added component

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+const autoprefixer = require("autoprefixer");
+
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    container: {
+      center: true,
+      padding: '2rem',
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
